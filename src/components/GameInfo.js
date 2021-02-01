76f83bc2921b8586292aadddbdc34dffa692b1a8
Laurence Rega <regalaurence@gmail.com>
2021-02-01 14:07:27 +0100

    corrected english

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -15,11 +15,11 @@ const GameInfo = (props) => {
                 <div className="status winner">It's a tie! </div>
                 :
                 props.isGameWon ?
-                    <div className="status winner">Player {props.isPlayerTwoNext ? "one" : "two"} won !</div>
+                    <div className="status winner">Player {props.isPlayerTwoNext ? "one" : "two"} won!</div>
                     :
                     <div className="status winner-tease">Who's gonna win this game?</div>
             }
-                <div className="status">{!props.isGameOver ? props.isPlayerOneNext ? "Player One is next, with X" : "Player Two is next, with O" : "Over. Refresh to play again!"}</div>
+                <div className="status">{!props.isGameOver ? props.isPlayerOneNext ? "Player One is next, with X" : "Player Two is next, with O" : "Over. Refresh to play again."}</div>
             </div>
         </div>
     );
